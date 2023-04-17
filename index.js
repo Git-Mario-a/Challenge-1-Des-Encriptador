83f340df1e263384f6e91679e12360d33b13ba88
Mario <mario.arturo.go@gmail.com>
2023-04-16 20:06:24 -0600

    hubieron montnal de cambios, el archivo principal se cambio al nombre index para seguir con el padron, ya quedo funcionando el encriptador y des encriptador pero esta todo feo, despues se haran los cambios correspondientes para eso, el anterior commit para guardar estos mismos cambios como que fallo por algo que movi al git hub pero por suerte tenia una copia mas o menos de lo que habia hecho

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-function performAction(action) {
-    const input = document.getElementById("input").value;
+function accion(action) {
+      const input = document.getElementById("input").value;
     let output;
 
-    if (action === "encrypt") {
-        output = encrypt(input);
-    } else if (action === "decrypt") {
-        output = decrypt(input);
+    if (action === "encriptar") {
+        output = encriptar(input);
+    } else if (action === "desencriptar") {
+        output = desencriptar(input);
     }
+
     document.getElementById("output").value = output;
 }
 
-function encrypt(input) {
+function encriptar(input) {
     const replaced = input.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     return replaced;
-}
-function decrypt(input) {
+  }
+  function desencriptar(input) {
     const replaced = input.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     return replaced;
-}
+  }
 
 function copyToClipboard() {
-const output = document.getElementById("output");
-output.select();
-output.setSelectionRange(0, 99999);
-document.execCommand("copy");
-alert("Texto copiado al portapapeles!");
+  const output = document.getElementById("output");
+  output.select();
+  output.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Texto copiado al portapapeles!");
 }
